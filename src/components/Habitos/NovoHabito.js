9c001e0d3ae6772118f5a05146080d1f0eef4d62
DiogoAAbreu <diogoalberto.dev@gmail.com>
2024-09-04 15:14:53 -0300

    instalado dayjs e iniciado estilizacao da funcao add habito

--- src/components/Habitos/NovoHabito.js
+++ src/components/Habitos/NovoHabito.js
@@ -3,14 +3,36 @@ import styled from "styled-components"
 export default function NovoHabito() {
     return (
         <NovoHabitoWrapper>
-
+            <form>
+                <input placeholder="nome do hábito" />
+            </form>
         </NovoHabitoWrapper>
     )
 }
 
 const NovoHabitoWrapper = styled.div`
-background-color: #FFFFFF;
-width: 340px;
-height: 180px;
-border-radius: 5px;
+input{
+    width: 303px;
+    height: 45px;
+    font-size: 20px;
+    padding: 8px;
+    border: solid 1px #D4D4D4;
+    border-radius:5px;
+
+    &::placeholder{
+        color:#DBDBDB;
+    }
+}
+
+&&{
+    padding: 25px;
+    display: flex;
+    align-items: start;
+    justify-content: center;
+    background-color: #FFFFFF;
+    width: 340px;
+    height: 180px;
+    border-radius: 5px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    }
 `
